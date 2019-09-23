123382a7d3d87caa997f45955d116bba3ce8f770
saikoka <46795444+saikoka@users.noreply.github.com>
2019-09-23 00:46:19 -0400

    Add files via upload

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,6 +1,7 @@
 
 from astar_euclidean import *
 from astar_manhattan import *
+from bfs import *
 import random
 dimension_size = int(input())  # Asks the user for the dimension size of the map and converts the string value to an int
 p_value = float(input())  # Asks the user for the probability and converts the string value to a float
@@ -20,7 +21,7 @@ def map_creator(dim, p):  # Function that returns initial map
     return map
 
 
-print(euclidean(map_creator(dimension_size,p_value)))
+print(map_bfs(map_creator(dimension_size,p_value)))
 
 
 
